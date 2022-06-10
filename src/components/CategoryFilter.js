95af50a50014f46cb89e830c979711966fab4532
Lyn Finman <lynfinman@gmail.com>
2022-06-10 12:57:05 -0400

    Notifying app when selected category changes

--- src/components/CategoryFilter.js
+++ src/components/CategoryFilter.js
@@ -1,10 +1,30 @@
 import React from "react";
 
-function CategoryFilter() {
+
+
+function CategoryFilter({ categories, onSelectCategory }) {
+
+  function handleClick(e) {
+    const allButtons = e.target.parentNode.querySelectorAll('button')
+    allButtons.forEach(
+      button =>
+        button.innerText === e.target.innerText ?
+          button.className = 'selected' :
+          button.className = ''
+    );
+    onSelectCategory(e.target.innerText);
+  }
   return (
     <div className="categories">
       <h5>Category filters</h5>
-      {/* render <button> elements for each category here */}
+      {
+        categories.map((category) =>
+          <button
+            onClick={handleClick}
+            className=""
+            key={category}
+          >{category}</button>)
+      }
     </div>
   );
 }
